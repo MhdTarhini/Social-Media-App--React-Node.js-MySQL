8c849459ddc920c704f4ed7f0c076e280cd6a4ac
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-19 15:47:33 +0300

    update

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -20,12 +20,12 @@ const Post = ({ post }) => {
       <div className="container">
         <div className="user">
           <div className="userInfo">
-            <img src={post.profilePic} alt="" />
+            <img src={post.user.profileImage} alt="" />
             <div className="details">
               <Link
                 to={`/profile/${post.userId}`}
                 style={{ textDecoration: "none", color: "inherit" }}>
-                <span className="name">{post.name}</span>
+                <span className="name">{post.user.name}</span>
               </Link>
               <span className="date">
                 {moment(post.createdAt, "YYYYMMDD").fromNow()}
--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,8 +1,9 @@
 const PostsModel = require("../models/posts");
+const userModels = require("../models/users");
 
 const getPosts = async (req, res) => {
   try {
-    const allPosts = await PostsModel.findAll();
+    const allPosts = await PostsModel.findAll({ include: [userModels] });
     res.status(200).json(allPosts);
   } catch (error) {
     res.status(404).json(error);
